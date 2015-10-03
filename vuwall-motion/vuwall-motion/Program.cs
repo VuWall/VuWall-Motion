004d3e41a07aa5c41059cfe5dd8d175c9f465d00
Justin Sarwer <Justins@vuwall.com>
2015-10-02 22:29:45 -0400

    Guud formatting

--- vuwall-motion/vuwall-motion/Program.cs
+++ vuwall-motion/vuwall-motion/Program.cs
@@ -4,16 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace vuwall_motion
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
+namespace vuwall_motion {
+    static class Program {
+        static void Main() {
 
         }
     }
