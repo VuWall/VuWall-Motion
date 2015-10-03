c698c9f86602ed39fea673dee43e0a0724f71fc6
David Lusignan <davidl@vuwall.com>
2015-10-03 13:17:07 -0400

    Remove stuff from Main

--- vuwall-motion/vuwall-motion/Program.cs
+++ vuwall-motion/vuwall-motion/Program.cs
@@ -20,74 +20,7 @@ namespace vuwall_motion
         [STAThread]
         static void Main()
         {
-            Point? absolute = null;
-
-            using (var channel = Channel.Create(ChannelDriver.Create(ChannelBridge.Create(), MyoErrorHandlerDriver.Create(MyoErrorHandlerBridge.Create()))))
-            {
-                using (var hub = Hub.Create(channel))
-                {
-                    hub.MyoConnected += (sender, e) =>
-                    {
-                        e.Myo.Unlock(UnlockType.Hold);
-                        e.Myo.Unlock(UnlockType.Timed);
-                        e.Myo.GyroscopeDataAcquired += (o, args) =>
-                        {
-                            var myo = (Myo)o;
-                            if (absolute == null)
-                            {
-                                absolute = Math3D.PixelFromVector(Math3D.DirectionalVector(Math3D.FromQuaternion(myo.Orientation)));
-                            }
-                            var position = Math3D.PixelFromVector(Math3D.DirectionalVector(Math3D.FromQuaternion(myo.Orientation)));
-                            position.Offset(0 - absolute.Value.X, 0 - absolute.Value.Y);
-                            if (position.X < 0)
-                            {
-                                position.X = 0;
-                            }
-                            else if (position.X > 1920)
-                            {
-                                position.X = 1920;
-                            }
-                            if (position.Y < 0)
-                            {
-                                position.Y = 0;
-                            }
-                            else if (position.Y > 1080)
-                            {
-                                position.Y = 1080;
-                            }
-                            Cursor.Position = position;
-                            
-                        };
-                        e.Myo.PoseChanged += (o, args) =>
-                        {
-                            var myo = (Myo)o;
-
-                            if (myo.Pose == Pose.Fist)
-                            {
-                                var api = new WindowApi();
-                                var position = Math3D.PixelFromVector(Math3D.DirectionalVector(Math3D.FromQuaternion(myo.Orientation)));
-                                var window = api.WindowFromPoint(position);
-                                var newWindow = new Window(window.Ptr, new Rectangle(0, 0, 600, 600));
-                                api.SetWindow(newWindow);
-                                api.BringToFront(newWindow);
-                            }
-
-                            if (!myo.IsUnlocked)
-                            {
-                                myo.Unlock(UnlockType.Hold);
-                                myo.Unlock(UnlockType.Timed);
-                            }
-                        };
-                    };
-
-                    channel.StartListening();
-
-                    while (true)
-                    {
-
-                    }
-                }
-            }
+            
         }
     }
 }
