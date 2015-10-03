5f46310117eb34c66277e603463904df0e0dd810
Darrel <ddayy.guerrero@hotmail.com>
2015-10-03 03:07:54 -0400

    Add Device Listening Tests

---
+++ vuwall-motion/TEst/MyoIntegrationTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using MyoSharp.Communication;
+using MyoSharp.Device;
+using MyoSharp.Discovery;
+using MyoSharp.Exceptions;
+using NUnit.Framework;
+
+namespace TEst
+{
+    [TestFixture]
+    internal class MyoIntegrationTests
+    {
+        [TestCase]
+        public void DiscoverMyoListener()
+        {
+            var channel = new Mock<IChannelListener>();
+            var result = DeviceListener.Create(channel.Object);
+
+            Assert.NotNull(result);
+        }
+
+        [TestCase]
+        public void DiscoverNullMyoListener()
+        {   
+            var exception = Assert.Throws<ArgumentNullException>(() => DeviceListener.Create(null));
+            Assert.That(exception.ParamName, Is.EqualTo("channelListener"));     
+        }
+    }
+}
