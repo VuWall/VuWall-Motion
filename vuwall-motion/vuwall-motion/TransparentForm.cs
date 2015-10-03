874576d8d166393696765bd60867c72c8a14582c
Justin Sarwer <Justins@vuwall.com>
2015-10-03 03:27:10 -0400

    Fix bug where drawn objects are also transparent, and add pen variable.

--- vuwall-motion/vuwall-motion/TransparentForm.cs
+++ vuwall-motion/vuwall-motion/TransparentForm.cs
@@ -4,9 +4,13 @@ using System.Windows.Forms;
 
 namespace vuwall_motion {
     public partial class TransparentForm : Form {
+        private Pen pen = new Pen(Color.Red, 5);
+
         public TransparentForm() {
             InitializeComponent();
             DoubleBuffered = true;
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            TransparencyKey = BackColor;
             ShowInTaskbar = false;
         }
 
@@ -14,13 +18,12 @@ namespace vuwall_motion {
         {
             int wl = TransparentWindowAPI.GetWindowLong(this.Handle, TransparentWindowAPI.GWL.ExStyle);
             wl = wl | 0x80000 | 0x20;
-            TransparentWindowAPI.SetWindowLong(this.Handle, TransparentWindowAPI.GWL.ExStyle, wl);
             TransparentWindowAPI.SetLayeredWindowAttributes(this.Handle, 0, 128, TransparentWindowAPI.LWA.Alpha);
             Invalidate();
         }
 
         private void TransparentForm_Paint(object sender, PaintEventArgs e) {
-            e.Graphics.DrawEllipse(Pens.Red, 250, 250, 20, 20);
+            e.Graphics.DrawEllipse(pen, 250, 250, 20, 20);
         }
 
         // TODO: Method to get an event from MYO to get x & y positions, used to invalidate
