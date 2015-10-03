3b22cd6ba88548de1878696d91abb9dcb3f8305e
David Lusignan <davidl@vuwall.com>
2015-10-02 22:01:18 -0400

    Remove Form and stuff

--- vuwall-motion/vuwall-motion/Program.cs
+++ vuwall-motion/vuwall-motion/Program.cs
@@ -14,9 +14,7 @@ namespace vuwall_motion
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
         }
     }
 }
