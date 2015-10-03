04c484f7eb707fd83c3f042c22462fa2341677b2
David Lusignan <davidl@vuwall.com>
2015-10-02 22:24:13 -0400

    Add a WindowApi with Window object and a test class

---
+++ vuwall-motion/Test/WindowsAPI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using vuwall_motion;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Test
+{
+    /// <summary>
+    /// Summary description for WindowsAPI
+    /// </summary>
+    [TestClass]
+    public class WindowsAPI : AssertionHelper
+    {
+
+        [TestMethod]
+        public void GetWindows()
+        {
+        }
+    }
+}
