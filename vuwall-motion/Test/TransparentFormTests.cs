affb789e9e728622c8bedd26ef21ab9863af6101
Justin Sarwer <Justins@vuwall.com>
2015-10-02 22:55:40 -0400

    WIP

---
+++ vuwall-motion/Test/TransparentFormTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using vuwall_motion;
+
+namespace Test {
+    public class TransparentFormTests : AssertionHelper {
+        [SetUp]
+        public void SetUp()
+        {
+            var form = new TransparentForm();
+            form.Show(null);
+        }
+
+        public void Basic() {
+            
+        }
+    }
+}
---
+++ vuwall-motion/vuwall-motion/TransparentForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vuwall_motion {
+    public partial class TransparentForm : Form {
+        public TransparentForm() {
+            InitializeComponent();
+            Opacity = 1;
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void TransparentForm_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
